9f9f50f1d0cb730adf9069ab483ee9a4e6fc434c
vanya-antonov <vanya.antonov@gmail.com>
2019-07-16 19:47:14 +0300

    all_orgs_hist.pdf -- the distribution of chlD genes by phyla

--- data_R/orgs.R
+++ data_R/orgs.R
@@ -7,7 +7,15 @@ source("../images_R/lib.R")
 
 ###
 
-orgs_all_df <- dbGetQuery(con, "select id, kingdom, phylum, genus, name from orgs order by name")
+SQL <- "
+select o.id, o.name, o.genus, o.phylum, o.kingdom,
+    (select t.value from org_params t where t.parent_id=o.id and t.name='taxonomy' and t.num=2) AS tax2,
+    (select count(distinct t.id) from chel_feats_v t where t.org_id=o.id and t.chel_subunit='M') AS num_M,
+    (select count(distinct t.id) from chel_feats_v t where t.org_id=o.id and t.chel_gene in ('chlD', 'bchD')) AS num_bchlD,
+    (select count(distinct t.id) from chel_feats_v t where t.org_id=o.id and t.chel_gene in ('chlD', 'bchD') and t.fs_len in (-1, 1)) AS num_bchlD_fs
+from orgs o;
+"
+orgs_all_df <- dbGetQuery(con, SQL)
 write.table(orgs_all_df, file = paste0(DATA_DIR, "orgs_all.txt"), row.names = FALSE, quote = FALSE, sep = "\t")
 
 orgs_chel_df <- dbGetQuery(con, "select id, kingdom, phylum, genus, name, num_M_zero, num_M_minus, num_M_plus from chel_orgs_v order by name")
--- images_R/all_orgs_hist.R
+++ images_R/all_orgs_hist.R
@@ -2,33 +2,51 @@
 source("lib.R")
 
 ###
-
 # Histogram by phylum ----
 
-# df <- dbGetQuery(con, "select phylum, kingdom, num_bchlD_zero, num_bchlD_minus, num_bchlD_plus from chel_orgs_v")
-df <- dbGetQuery(con, "select phylum, kingdom, num_M_zero, num_M_minus, num_M_plus from chel_orgs_v")
+orgs_df <- read.delim(paste0(DATA_DIR, "orgs_all.txt"), as.is=TRUE) %>%
+  mutate(org_type = case_when(num_bchlD_fs > 0 ~ 'bchlD_fs',
+                              num_bchlD > 0 ~ 'bchlD_wo_fs',
+                              num_bchlD == 0 ~ 'no_bchlD'))
+head(orgs_df, n = 20)
 
-most_frequent <- c("Actinobacteria", "Proteobacteria", "Archaea", "Cyanobacteria", "Firmicutes", "Chloroflexi", 'Other')
-df <- df %>%
-  mutate(phylum = ifelse(kingdom == 'Archaea', 'Archaea', phylum)) %>%
-  mutate(phylum = ifelse(phylum %in% most_frequent, phylum, 'Other'))
+most_frequent <- c("Proteobacteria", "Actinobacteria", "Archaea", "Chloroflexi", "Firmicutes", "Cyanobacteria", 'Other')
+#most_frequent <- c("Proteobacteria", "Actinobacteria", "Archaea", "Firmicutes", "Cyanobacteria", 'Other')
 
-gg_df <- df %>%
-  group_by(phylum) %>%
-  summarise('None' = sum(num_M_zero),
-            '+1' = sum(num_M_plus),
-            '-1' = sum(num_M_minus)) %>%
-  # summarise('None' = sum(num_bchlD_zero),
-  #           '+1' = sum(num_bchlD_plus),
-  #           '-1' = sum(num_bchlD_minus)) %>%
-  gather(Frameshift, num, -phylum)
+ORG_TYPE_COLORS <- c(
+  'bchlD_fs' = 'red',
+  'bchlD_wo_fs' = 'lightgreen',
+  'no_bchlD' = 'grey')
+ORG_TYPE_LABELS <- c(
+  'bchlD_fs' = 'Frameshifted chlD gene(s)',
+  'bchlD_wo_fs' = 'Normal chlD gene(s)',
+  'no_bchlD' = 'No chlD gene(s)')
 
-#RColorBrewer::display.brewer.all()
-ggplot(gg_df) + 
-  aes(x = factor(phylum, levels = rev(most_frequent)), y = num, fill = Frameshift) +
-  geom_bar(stat = 'identity', col = 'black') + 
-  ylab('Number of medium subunit genes') +
-  xlab('Phylum') +
-  #  scale_fill_brewer(palette = "Set1") +
-  coord_flip()
-ggsave('all_orgs_hist.pdf', path = OUT_DIR, width = 7)
+orgs_df %>%
+  mutate(taxa = case_when(kingdom == 'Archaea' ~ 'Archaea',
+                          phylum %in% most_frequent ~ phylum,
+                          TRUE ~ 'Other')) %>%
+  # Define the order of phyla
+  mutate(taxa = factor(taxa, levels = rev(most_frequent))) %>%
+  # Define the order of org_types
+  mutate(org_type = factor(org_type, levels = rev(names(ORG_TYPE_COLORS)))) %>%
+  ggplot() +
+  aes(x = taxa, fill = org_type) +
+  geom_bar(col = 'black') +
+  ylab('Number of species') +
+  xlab('') +
+  ggtitle(paste(nrow(orgs_df), 'prokaryotic species'),
+          subtitle = 'NCBI Reference and Representative genomes') +
+  scale_fill_manual(
+    name = NULL,
+    values = ORG_TYPE_COLORS,
+    breaks = names(ORG_TYPE_COLORS),
+    labels = ORG_TYPE_LABELS) +
+  # Legend items in one column
+#  guides(fill=guide_legend(ncol=1)) +
+  coord_flip() +
+  theme(legend.position="bottom")
+ggsave('all_orgs_hist.pdf', path = OUT_DIR)
+#ggsave('all_orgs_hist.pdf', path = OUT_DIR, width = 7)
+
+#?scale_fill_manual
