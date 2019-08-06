65f50adf18f234cd6c010c74508c7a49ff276266
vanya-antonov <vanya.antonov@gmail.com>
2019-08-06 17:31:10 +0300

    genotype_tree_Proteobacteria

--- images_R/genotype_tree.R
+++ images_R/genotype_tree.R
@@ -44,7 +44,7 @@ Burkholderia_node <- tar_orgs_df %>%
 title <- paste(nrow(tar_orgs_df), tar_phylum, 'species with exactly 1 chlD gene')
 
 ggtree(tar_tree) %>%
-  gheatmap(small_genotype, offset=0.3, width=0.2, colnames_offset_y = -1.5) +
+  gheatmap(small_genotype, offset=0.3, width=0.5, colnames_offset_y = -1.5) +
   #    geom_tiplab(size=2, align=TRUE, linesize=.5) +
   scale_fill_manual(values=c("None"="black", "-1"="blue", "+1"="red")) +
   geom_cladelabel(node=Pseudomonas_node, label="Pseudomonas") +
@@ -52,3 +52,21 @@ ggtree(tar_tree) %>%
   ggtitle(title)
 ggsave('genotype_tree_Proteobacteria.pdf', path = OUT_DIR,
        width = 7, height = 5)
+
+
+# Statistics
+# Frameshift in chlD   |   chlI gene is absent
+# Y | Y = 65
+YY <- tar_orgs_df %>% filter(num_M_fs > 0 & num_S == 0) %>% nrow()
+# Y | N = 0
+YN <- tar_orgs_df %>% filter(num_M_fs > 0 & num_S > 0) %>% nrow()
+# N | Y = 38
+NY <- tar_orgs_df %>% filter(num_M_fs == 0 & num_S == 0) %>% nrow()
+# N | N = 14
+NN <- tar_orgs_df %>% filter(num_M_fs == 0 & num_S > 0) %>% nrow()
+
+
+mtx <- matrix(c(YY, YN,
+                NY, NN),
+              ncol=2, byrow=TRUE)
+fisher.test(mtx)
