fd1c0090d881aafdee86c91e8faa092102809b9c
vanya-antonov <vanya.antonov@gmail.com>
2019-08-19 16:51:55 +0300

    org_genotypes image was updated

--- images_R/org_genotypes.R
+++ images_R/org_genotypes.R
@@ -3,71 +3,76 @@ source("lib.R")
 
 ###
 
-NUM_L_COLORS <- c(
-  '0' = 'white',
-  '1' = 'lightgreen',
-  '2' = 'orange',
-  '>3' = 'red')
+# TAR_PHYLA <- c('Proteobacteria', 'Actinobacteria', 'Chloroflexi',  'Euryarchaeota')
+TAR_PHYLA <- c('Proteobacteria', 'Actinobacteria', 'Euryarchaeota')
 
 ###
 
-orgs_df <- read.delim(paste0(DATA_DIR, "org_genotypes.txt"), as.is=TRUE)
+all_orgs_df <- read.delim(paste0(DATA_DIR, "orgs_chel.txt"), as.is=TRUE)
 head(orgs_df)
 
 # Generate the genotype column
-orgs_df <- orgs_df %>%
+orgs_df <- all_orgs_df %>%
+  filter(phylum %in% TAR_PHYLA) %>%
   mutate(num_M_zero = num_M - num_M_fs) %>%
   mutate(L_str = ifelse(num_L == 0, NA, paste0(num_L, 'xL')),
-         M_0_str = ifelse(num_M_zero == 0, NA, paste0(num_M_zero, 'xM_0')),
-         M_fs_str = ifelse(num_M_fs == 0, NA, paste0(num_M_fs, 'xM_fs')),
+         M_0_str = ifelse(num_M_zero == 0, NA, paste0(num_M_zero, 'xM')),
+         M_fs_str = ifelse(num_M_fs == 0, NA, paste0(num_M_fs, 'xfs_chlD')),
          S_str = ifelse(num_S == 0, NA, paste0(num_S, 'xS'))) %>%
-  unite(L_str, M_0_str, M_fs_str, S_str, sep = ',', col="genotype") %>%
+  unite(L_str, M_0_str, S_str, M_fs_str, sep = ',', col="genotype") %>%
   # Remove NA from genotypes using regexp:
   # '1xL,1xM_0,NA,NA'   =>  '1xL,1xM_0'
   # '3xL,1xM_0,NA,1xS'  =>  '3xL,1xM_0,1xS'
   mutate(genotype = gsub('(,NA)+', '', genotype))
-
-
-genotype_histogram_gg <- function(gg_df, top_genotypes)
-{
-  top_genotypes <- c(top_genotypes, 'Other')
-  title <- sprintf('Total number of genomes = %d', nrow(gg_df))
-  subT <- sprintf('# chlD genes = %d (with fs) + %d (without fs) = %d',
-                  sum(gg_df$num_M_fs), sum(gg_df$num_M_zero), sum(gg_df$num_M))
-  gg_df %>%
-    mutate(genotype = ifelse(genotype %in% top_genotypes, genotype, 'Other')) %>%
-    # Define the order
-    mutate(genotype = factor(genotype, levels = rev(top_genotypes))) %>%
-    mutate(num_L_str = ifelse(num_L >= 3, '>3', as.character(num_L))) %>%
-    # Define the order
-    mutate(num_L_str = factor(num_L_str, levels = c('0', '1', '2', '>3'))) %>%
-    ggplot() +
-    aes(x = genotype, fill = num_L_str) +
-    geom_bar(col = 'black') +
-    ylab('Number of genomes') +
-    xlab('') +
-    ggtitle(title, subtitle = subT) +
-    scale_fill_manual(
-      name = '# L-genes\nin genotype',
-      values = NUM_L_COLORS) +
-    coord_flip()
-}
-
-PHYLUM = "Proteobacteria"
-TOP_GENOTYPES_PROTEO <- c("2xL,1xM_0,1xM_fs,1xS", "1xL,1xM_fs", "1xL,1xM_0",
-                          "1xL,1xM_0,1xM_fs,1xS", "2xL,2xM_0,1xM_fs,1xS","1xL,1xM_0,1xS")
+head(orgs_df)
 
 orgs_df %>%
-  filter(phylum == PHYLUM) %>%
-  genotype_histogram_gg(TOP_GENOTYPES_PROTEO)
-ggsave(paste0('org_genotypes_', PHYLUM, '.pdf'), path = OUT_DIR)
+  group_by(phylum, genotype) %>%
+  summarise(n = n()) %>%
+  arrange(phylum, -n) %>%
+  as.data.frame()
 
+# Expected
+EXPECTED_G <- c(
+  '1xL,1xM,1xS',    # Expected
+  '2xL,2xM,2xS',    # Expected
+  '1xL,2xM,2xS',            # Proteobacteria
+  
+  # with fs_chlD
+  '1xL,1xfs_chlD',  # Expected
+  '2xL,1xfs_chlD',   # Euryarchaeota (M.f.), Proteobacteria
+  '3xL,1xfs_chlD',   # Euryarchaeota (M.s.)
+  '2xL,1xM,1xS,1xfs_chlD'  # Proteobacteria
+)
 
-# Most frequent genotype
-# orgs_df %>%
-#   filter(phylum == PHYLUM) %>%
-#   group_by(genotype) %>%
-#   summarise(n_orgs = n()) %>%
-#   arrange(-n_orgs) %>%
-#   head(n = 6) %>%
-#   pull(genotype)
+NOT_EXPECTED_G <- c(
+  # without fs_chlD
+  '1xL,1xM',   # Actinobacteria, Euryarchaeota, Proteobacteria
+  '1xL,2xM',   # Actinobacteria
+  '1xL,1xM,1xS,1xfs_chlD',  # Proteobacteria (D.a.)
+  'Other'
+)
+
+TOP_GENOTYPES <- c(EXPECTED_G, NOT_EXPECTED_G)
+
+orgs_df %>%
+  mutate(genotype = ifelse(genotype %in% TOP_GENOTYPES, genotype, 'Other')) %>%
+  # Define the order
+  mutate(genotype = factor(genotype, levels = rev(TOP_GENOTYPES))) %>%
+  mutate(phylum = factor(phylum, levels = TAR_PHYLA)) %>%
+  ggplot() +
+  aes(x = genotype, fill = ifelse(num_M_fs > 0, 'Yes', 'No')) +
+  geom_bar(col = 'black') +
+  ylab('Number of genomes') +
+  xlab('Chelatase genotype') +
+#  ggtitle(title, subtitle = subT) +
+  # scale_fill_manual(
+  #   name = 'fs-chlD gene',
+  #   values = c('red', 'gray')) +
+  scale_fill_manual(
+    name = 'Genotype contains fs-chlD gene: ',
+    values = c('No' = 'gray', 'Yes' = 'red')) +
+  theme(legend.position="top") +
+  coord_flip() +
+  facet_wrap(.~phylum, nrow = 1, scales = 'free_x')
+ggsave(paste0('org_genotypes.pdf'), path = OUT_DIR, w=10, h=5)
