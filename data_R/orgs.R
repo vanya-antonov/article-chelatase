6898f5e3195d674563f2a482af9dd79e578bacf9
vanya-antonov <vanya.antonov@gmail.com>
2019-10-08 18:51:57 +0300

    gene_id's were added to the suppl_data table ...
    and new genotypes were generated by using better thresholds for the cheltase subunit lengths

--- data_R/orgs.R
+++ data_R/orgs.R
@@ -17,6 +17,3 @@ from orgs o;
 "
 orgs_all_df <- dbGetQuery(con, SQL)
 write.table(orgs_all_df, file = paste0(DATA_DIR, "orgs_all.txt"), row.names = FALSE, quote = FALSE, sep = "\t")
-
-orgs_chel_df <- dbGetQuery(con, "select id, kingdom, phylum, genus, name, num_M_zero, num_M_minus, num_M_plus from chel_orgs_v order by name")
-write.table(orgs_chel_df, file = paste0(DATA_DIR, "orgs_chel.txt"), row.names = FALSE, quote = FALSE, sep = "\t")
--- data_R/suppl_data_orgs.R
+++ data_R/suppl_data_orgs.R
@@ -4,17 +4,30 @@ source("../images_R/lib.R")
 
 ###
 
-
-all_orgs_df <- read.delim(paste0(DATA_DIR, "orgs_chel.txt"), as.is=TRUE)
+all_orgs_df <- read.delim(paste0(DATA_DIR, "orgs_chel.txt"), as.is=TRUE) %>%
+  # Generate the genotype column
+  make_genotype_column()
 head(all_orgs_df)
 
-# Generate the genotype column
-orgs_df <- make_genotype_column(all_orgs_df)
-head(orgs_df)
+all_genes_df <- read.delim(paste0(DATA_DIR, "genes_chel.txt"), as.is=TRUE) %>%
+  # Use 'chlIDH' for 'bchIDH'
+  mutate(chel_gene = case_when(chel_gene == 'bchI' ~ 'chlI',
+                               chel_gene == 'bchD' ~ 'chlD',
+                               chel_gene == 'bchH' ~ 'chlH',
+                               TRUE ~ chel_gene))
+unique(all_genes_df$chel_gene)
+head(all_genes_df)
+
+# Concatenate gene IDs and merge with orgs
+orgs_df <- all_genes_df %>%
+  group_by(org_name, chel_gene) %>%
+  summarise(gene_id = paste(name, collapse=", ")) %>%
+  spread(chel_gene, gene_id, fill = '-') %>%
+  inner_join(all_orgs_df, by= c('org_name' = 'name'))
 
 orgs_df %>%
-  select(name, kingdom, phylum, tax2, num_M_fs, genotype) %>%
+  select(org_name, kingdom, phylum, tax2, num_M_fs, genotype, cobN, cobT, cobS, chlH, chlD, chlI) %>%
   rename(num_fs_chlD = num_M_fs) %>%
-  arrange(name) %>%
+  arrange(org_name) %>%
   write.table(file = paste0(DATA_DIR, "suppl_data_orgs.txt"),
               row.names = FALSE, quote = FALSE, sep = "\t")
