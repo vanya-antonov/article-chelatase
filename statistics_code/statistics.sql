e2b982fa27100376ce2b1d8cd2c0cf4b650d64cd
vanya-antonov <vanya.antonov@gmail.com>
2019-10-16 16:05:10 +0300

    statistics for Archaea

--- statistics_code/statistics.sql
+++ statistics_code/statistics.sql
@@ -9,3 +9,7 @@ select * from chel_orgs_v where phylum = 'Actinobacteria' and genotype = '1xcobN
 # "More than half of the actinobacterial genomes with this genotype were from the Streptomyces genera."
 select * from chel_orgs_v where genus = 'Streptomyces' and genotype = '1xcobN, 1xchlD';
 
+# "out of the 25 archaeal genomes with frameshifted chlD genes, 23 had the "N x L, 1 x fs-chlD" genotype"
+select * from chel_orgs_v where kingdom = 'Archaea' and num_M_fs > 0;
+select * from chel_orgs_v where kingdom = 'Archaea' and num_M_fs > 0 and num_L > 0 and num_S=0;
+
